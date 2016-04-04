1b21d24083e65bfdbf6b8fbddee1c8c53a6d1bb9
Ankit Ati <ankmonuati@gmail.com>
2016-04-04 22:35:13 +0530

    Profile display changes

---
+++ src/main/java/com/app/factory/ProfileDAO.java
@@ -0,0 +1,78 @@
+package com.app.factory;
+
+import java.util.List;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import com.app.exception.ApplicationException;
+import com.app.factory.beans.User;
+import com.app.factory.beans.UserDescription;
+
+/**
+ * This class provides the database related functionalities for profile viewing and editing.
+ * @author Ankit
+ * @version 1.0
+ */
+
+public class ProfileDAO {
+
+	private static final Logger logger = LogManager.getLogger(ProfileDAO.class);
+	private String username;
+	
+	public ProfileDAO(String username) {
+		this.username = username;
+	}
+	
+	/**
+	 * This method returns the user description of the user with a particular username.
+	 * In case the username does not exists, it returns null indicating that something is not right.
+	 * @return user description
+	 * @throws ApplicationException
+	 */
+	@SuppressWarnings("unchecked")
+	public UserDescription getUserDetails() throws ApplicationException {
+		Session session = Connection.getInstance().openSession();
+		if(session == null) {
+			throw new ApplicationException("Connection not initiated.");
+		}
+		Transaction transaction = null;
+		String hql =  "Select UserDescription as d FROM d join d.user as user "
+				+ "Where user.username = :username";
+		
+		try {
+			transaction = session.beginTransaction();
+			List<UserDescription> list = session.createQuery(hql).setParameter("username", this.username).list();
+			transaction.commit();
+			
+			//System.out.println(list.size());
+			if(list.size()>1) {
+				throw new ApplicationException("More than one users found in constructor of ProfileDAO.");
+			} else if(list.size()<=0) {
+				throw new ApplicationException("No users found with the provided details");
+			}
+			System.out.println(list.get(0) instanceof UserDescription);
+			return (UserDescription)list.get(0);
+		} catch(HibernateException ex) {
+			System.out.println(ex);
+			logger.debug("Exception caught in Constructor of ProfileDAO. " + ex);
+			if(transaction!=null) {
+				transaction.rollback();
+			}
+			return null;
+		} catch(Exception ex) {
+			ex.printStackTrace();
+			logger.debug("Exception Caught in constructor of ProfileDAO. " + ex);
+			if(transaction!=null) {
+				transaction.rollback();
+			}
+			return null;
+		} finally {
+			session.close();
+		}
+	}
+	
+}
---
+++ src/main/java/com/app/service/ProfileService.java
@@ -0,0 +1,36 @@
+package com.app.service;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import com.app.exception.ApplicationException;
+import com.app.factory.ProfileDAO;
+import com.app.factory.beans.UserDescription;
+
+/**
+ * This class provides the services related to the Profile of an User. Editing the profile, viewing the profile and others are managed by this class.
+ * @author Ankit
+ * @version 1.0
+ */
+public class ProfileService {
+	public static final Logger logger = LogManager.getLogger(ProfileService.class);
+	private String username;
+	
+	public ProfileService(String username) {
+		this.username = username;
+	}
+	
+	/**
+	 * This method provides the implementation of getting the user details of the user with the specific username.
+	 * @return UserDescription of the user with the giver username
+	 * @throws ApplicationException
+	 */
+	public UserDescription fetchProfileDetails() throws ApplicationException {
+		logger.trace("Entering "+ProfileService.class + ".fetchProfileDetails method");
+		ProfileDAO db = new ProfileDAO(username);
+		UserDescription user = db.getUserDetails();
+		System.out.println(user);
+		user.setId(-1); //This user will be set in session or request. To remove the unnecessary information from the object.
+		return user;
+	}
+}
